d688f5bce7edb9663bd967d480930bdb715a140e
Ben-Hardy <ben@benjaminhardy.ca>
2018-07-18 19:25:44 -0600

    Added Ability to add Medication Log Entry
    Added a new activity that allows for adding of medications to the
    medication log database.  Next is to do view and delete for entries.
    That will be a recyclerview of some type I'm thinking. That's tomorrow's
    project

--- app/src/main/java/ben/medtracker/AddLogEntryActivity.java
+++ app/src/main/java/ben/medtracker/AddLogEntryActivity.java
@@ -1,13 +1,89 @@
 package ben.medtracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Date;
+
+import ben.medtracker.data.DateConverter;
+import ben.medtracker.data.MedicationLogDatabase;
+import ben.medtracker.data.MedicationLogEntry;
 
 public class AddLogEntryActivity extends AppCompatActivity {
+
+    public static final String LOG = AddLogEntryActivity.class.getSimpleName();
+    public static final String EXTRA_MED_NAME = "extramedname";
+    private static final String DATE_FORMAT = "dd/MM/yyy";
+
+    TextView medicationNameTextView;
+    EditText doseAmountTextView;
+    EditText notesTextView;
+
+    Button addEntryButton;
+
+    String medicationName;
+    Intent intent;
+
+    MedicationLogDatabase logDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_log_entry);
+
+        initializeUI();
+
+        logDb = MedicationLogDatabase.getDatabase(this);
+    }
+
+    public void initializeUI() {
+        medicationNameTextView = findViewById(R.id.add_entry_med_name_tv);
+        doseAmountTextView = findViewById(R.id.add_entry_num_doses_et);
+        notesTextView = findViewById(R.id.add_entry_notes_et);
+
+        intent = getIntent();
+        medicationName = intent.getStringExtra(EXTRA_MED_NAME);
+        medicationNameTextView.setText(medicationName);
+
+        addEntryButton = findViewById(R.id.add_entry_add_entry_button);
+        addEntryButton.setOnClickListener(onAddEntryButtonClickedListener());
+    }
+
+    public View.OnClickListener onAddEntryButtonClickedListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (doseAmountTextView.getText().toString().isEmpty()) {
+                    Toast.makeText(AddLogEntryActivity.this, R.string.add_entry_toast,
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    String dosage = doseAmountTextView.getText().toString();
+                    String notes;
+                    if (notesTextView.getText().toString().isEmpty()) {
+                        notes = "No notes added for this entry.";
+                    } else {
+                        notes = notesTextView.getText().toString();
+                    }
+
+                    final MedicationLogEntry entry = new MedicationLogEntry(medicationName, dosage, new Date(), notes);
+
+                    AppExecutors.getInstance().diskIO().execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            logDb.logDao().insertLogEntry(entry);
+                        }
+                    });
+                    Log.d(LOG, "Added new log entry");
+                    onBackPressed();
+                }
+            }
+        };
     }
 }
